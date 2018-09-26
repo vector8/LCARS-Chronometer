22384b42a0f334863b50724eab79be90d942266d
Chris <vector8@users.noreply.github.com>
2018-09-26 11:37:17 -0400

    First commit.

---
+++ StardateClock/Assets/StardateCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class StardateCalculator : MonoBehaviour
+{
+    Text text;
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        System.DateTime currentTime = System.DateTime.Now;
+
+        string stardate = currentTime.ToString("yyyyMMdd");
+        float hours = currentTime.Hour + (currentTime.Minute / 60f);
+        string hourString = (hours / 24f).ToString();
+        hourString = hourString.Substring(2, 2);
+        stardate += "." + hourString;
+
+        text.text = stardate;
+    }
+}
